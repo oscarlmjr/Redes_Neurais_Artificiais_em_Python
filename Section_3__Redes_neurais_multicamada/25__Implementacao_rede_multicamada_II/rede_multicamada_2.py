2563444e293575443ab88471089a5a38cdd827f0
oscarlmjr <oscarlmjr@gmail.com>
2026-07-05 00:36:42 -0300

    Atualizando secao_3 aula_25

--- Section_3__Redes_neurais_multicamada/25__Implementacao_rede_multicamada_II/rede_multicamada_2.py
+++ Section_3__Redes_neurais_multicamada/25__Implementacao_rede_multicamada_II/rede_multicamada_2.py
@@ -12,24 +12,27 @@ somaSinapse = 0
 def sigmoid(soma):
 	return 1 / (1 + np.exp(-soma))
 
-
 def soma_sinapse(entrada):
 	def interna(peso, indice_entrada):
 		somaSinapse = entrada[indice_entrada] * peso
-
 		return somaSinapse
+
 	return interna
 
 for entrada in entradas:
 	indice_peso = 0
 	variavel = soma_sinapse(entrada)
 	while indice_peso < len(pesos0[0]):
+		soma = 0
 		indice_pesos = 0
 		indice_entrada = 0
 		while indice_pesos < len(pesos0):
 			print('ind_pesos', indice_pesos, 'ind_peso', indice_peso, 
 			'ind_entrada', indice_entrada)
-			print(variavel(pesos0[indice_pesos][indice_peso], indice_entrada))
+			# print(variavel(pesos0[indice_pesos][indice_peso], indice_entrada))
+			funcao_sigmoide = variavel(pesos0[indice_pesos][indice_peso], indice_entrada)
+			soma += funcao_sigmoide
+			print(funcao_sigmoide)
 			indice_entrada += 1	
 			indice_pesos += 1
 			if indice_pesos == len(pesos0):
@@ -37,5 +40,5 @@ for entrada in entradas:
 				indice_entrada = 0
 		indice_peso += 1	
 
-	# print(sigmoid(somaSinapse))
+		print(sigmoid(soma))
 	print()
