a1b6864fbc65332d87131d2629d2e1dccef5e5c7
oscarlmjr <oscarlmjr@gmail.com>
2026-07-06 19:57:40 -0300

    secao_3 aula_27

--- Section_3__Redes_neurais_multicamada/27__Implementacao_rede_multicamada_III/rede_multicamada.py
+++ Section_3__Redes_neurais_multicamada/27__Implementacao_rede_multicamada_III/rede_multicamada.py
@@ -11,7 +11,7 @@ def sigmoid(soma):
 entradas = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 saidas = np.array([[0], [1], [1], [0]])
 pesos0 = np.array([[-0.424, -0.740, -0.961], [0.358, -0.577, -0.469]])
-pesos1 = np.array([[-0.017, -0.893, -0.148]])
+pesos1 = np.array([[-0.017, -0.893, 0.148]])
 
 epocas = 100
 
@@ -20,6 +20,6 @@ for j in range(epocas):
 	somaSinapse0 = np.dot(camadaEntrada, pesos0)
 	camadaOculta = sigmoid(somaSinapse0)
 	
-	somaSinapse1 = np.dot(camadaOculta, pesos1)
+	somaSinapse1 = np.dot(camadaOculta, pesos1.T)
 	camadaSaida = sigmoid(somaSinapse1)
 	
--- Section_3__Redes_neurais_multicamada/27__Implementacao_rede_multicamada_III/rede_multicamada_2.py
+++ Section_3__Redes_neurais_multicamada/27__Implementacao_rede_multicamada_III/rede_multicamada_2.py
@@ -6,6 +6,7 @@ import numpy as np
 
 entradas = [[0, 0], [0, 1], [1, 0], [1, 1]]
 pesos0 = [[-0.424, -0.740, -0.961], [0.358, -0.577, -0.469]]
+pesos1 = [[-0.017, -0.893, 0.148]]
 epocas = 100
 somaSinapse = 0
 
@@ -22,20 +23,27 @@ def soma_sinapse(entrada):
 for entrada in entradas:
 	indice_peso = 0
 	variavel = soma_sinapse(entrada)
+	somaSinapse1 = 0
 	while indice_peso < len(pesos0[0]):
-		soma = 0
+		somaSinapse0 = 0
 		indice_pesos = 0
 		indice_entrada = 0
 		while indice_pesos < len(pesos0):
-
-			funcao_sigmoide = variavel(pesos0[indice_pesos][indice_peso], indice_entrada)
-			soma += funcao_sigmoide
-			print(funcao_sigmoide)
+			camadaEntrada = variavel(pesos0[indice_pesos][indice_peso], 
+			indice_entrada)
+			somaSinapse0 += camadaEntrada
 			indice_entrada += 1	
 			indice_pesos += 1
 			if indice_pesos == len(pesos0):
 				indice_entrada = 0
-		indice_peso += 1	
-		print('soma', soma)
-		print(sigmoid(soma))
+		indice_peso += 1
+
+		camadaOculta = sigmoid(somaSinapse0) 
+		print('camadaOculta', camadaOculta)
+
+		somaSinapse1 += camadaOculta * pesos1[0][indice_peso - 1]
+		print('somaSinapse1', somaSinapse1)
+
+	camadaSaida = sigmoid(somaSinapse1)
+	print('camadaSaida', camadaSaida)
 	print()
